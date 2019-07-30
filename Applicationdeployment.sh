f52598095e8fa3589f73afda3cf96ff046e36fd1
Manoj240 <nmkumar240@gmail.com>
2019-07-30 08:40:41 +0530

    Added application deployment script

---
+++ Applicationdeployment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Setting prerequisties for Application deployment"
+sudo aws s3 cp s3://terraformmanoj/gitkeys/config /root/.ssh/config
+sudo aws s3 cp s3://terraformmanoj/gitkeys/id_rsa /root/.ssh/id_rsa
+sudo chmod 400 /root/.ssh/id_rsa
+
+sudo rm -rf /var/www/html
+
+echo "Downloading Application"
+sudo git clone -b master git@github.com:Manoj240/Manoj-static.git /var/www/html/
+
